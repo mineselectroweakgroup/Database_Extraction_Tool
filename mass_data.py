492959215a6294adee42fbc05325020b5c8b1df0
matmarti <matmarti@mines.edu>
2017-06-05 13:28:22 -0600

    Finishing merge

--- mass_data.py
+++ mass_data.py
@@ -36,12 +36,6 @@ def addMass(elementName,lowerBound,higherBound,wantedSpins):
 
                             for line in datafilelines:
                                 splitline = line.split(',')
-#<<<<<<< HEAD
-                                #if data[k][59]=='#':
-                                #    splitline[2] = splitline[2]+'*'
-                                #splitline[1] = str((float(splitline[1])-float(data[k][55:63].replace('#','.')))/1000)
-                                #unsplitline = splitline[0] + ',' + splitline[1] + ',' + splitline[2] + ',' + splitline[3]
-#=======
                                 N = int(data[k][6:9].replace(" ",""))
                                 Z = int(data[k][11:14].replace(" ",""))
                                 if data[k][106]=='#':
@@ -53,7 +47,6 @@ def addMass(elementName,lowerBound,higherBound,wantedSpins):
                                 splitline[1] = str(float(splitline[1])/10**6 + atomicMass)
                                 splitline[3] = str(float(splitline[3])/10**6)
                                 unsplitline = splitline[0] + ',' + splitline[1] + ',' + splitline[2]+','+splitline[3]+'\n'
-#>>>>>>> master
                                 datafile.write(unsplitline)
                 k=k+1
 
