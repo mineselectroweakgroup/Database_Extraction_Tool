773e2b740eff48b890bda5391b0b3eaa396d28c8
matmarti <matmarti@mines.edu>
2017-06-19 10:52:29 -0600

    Updated GUIs

--- renormalize.py
+++ renormalize.py
@@ -28,7 +28,6 @@ def renormalize(elementName,lowerBound,higherBound,wantedSpins):
             for line in datafilelines:
                 splitline = line.split(';')
                 splitline[1] = str(float(splitline[1])-float(minimum))
-                splitline[3] = splitline[3][:-1]
-                unsplitline = splitline[0] + ';' + splitline[1] + ';' + splitline[2] + ';' + splitline[3] + ';' + splitline[4] + '\n'
+                unsplitline = splitline[0] + ';' + splitline[1] + ';' + splitline[2] + ';' + splitline[3] + ';' + splitline[4]
                 datafile.write(unsplitline)
 
